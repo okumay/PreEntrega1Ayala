f69dcb0651b88cb4f64ecce1be698c006787d0a9
Sebastián Ayala <sebastian.ayala@wundermanthompson.com>
2023-05-23 12:55:52 -0300

    styling updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { ItemListContainer } from "./components/ItemListContainer";
 
 function App() {
   return (
-    <>
+    <div className="container-fluid">
       <NavBar></NavBar>
       <ItemListContainer greeting="Te damos la bienvenida al Emporio Retro!"></ItemListContainer>
-    </>
+    </div>
   );
 }
 
